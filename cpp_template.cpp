32b895d628715ccbe53955caae86b6ef1ef5d808
Hridoy Ahmed <99856822+hrid0yyy@users.noreply.github.com>
2025-09-06 18:13:14 +0600

    Enhance cpp_template.cpp with utility functions

--- cpp_template.cpp
+++ cpp_template.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+using namespace chrono;
 #define ll long long
 #define pb push_back
 #define all(x) (x).begin(), (x).end()
@@ -7,41 +8,60 @@ using namespace std;
 #define fr(i,s,n) for(int i = s; i < (n); i++)
 #define fre(x, v) for(auto& x : v)
 #define endl "\n"
-#define inf 1e18
 #define vi vector<int>
 #define vll vector<ll>
 #define pii pair<int, int>
 #define pll pair<ll, ll>
+#define vpii vector<pair<int, int>>
+#define mi map<int,int>
 #define ff first
 #define ss second
 
+const int INF = 1e9;
+const long long LINF = 1e18;
+const int MOD = 1e9 + 7;
 
-void precompute() {
-  
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "] = ", dout(__VA_ARGS__)
+void dout() { cerr << endl; }
+template <typename T, typename... Args>
+void dout(T t, Args... args) {
+    cerr << t;
+    if(sizeof...(args)) cerr << ", ";
+    dout(args...);
 }
 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll getRand(ll l, ll r) {
+    return uniform_int_distribution<ll>(l, r)(rng);
+}
 
 
 void soln() {
-  
-
+    
 }
+
+
 
 signed main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-  //  precompute();
-  //  freopen("input.txt", "r", stdin);
-  //  freopen("output.txt", "w", stdout);
-    int tc = 1;
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    auto start = high_resolution_clock::now(); 
+    ll tc = 1;
     cin >> tc;
-    
-    for (int cn = 1; cn <= tc; cn++)
-    {
+    for (ll cn = 1; cn <= tc; cn++) {
         soln();
     }
-  
+
+    auto end = high_resolution_clock::now();   
+    auto duration = duration_cast<milliseconds>(end - start).count();
+
+    cerr << "Execution Time: " << duration << " ms" << endl;
 
     return 0;
 }
